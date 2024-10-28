9538577ff259d204037c6547b4cd0dda7686fbe0
DevDAN09 <devyun09@gmail.com>
2024-10-28 14:13:24 +0900

    design: fixed mobile view on 768px

--- src/components/Frame/Frame.style.ts
+++ src/components/Frame/Frame.style.ts
@@ -3,14 +3,19 @@ import styled, { createGlobalStyle } from  "styled-components";
 export const GlobalStyles = createGlobalStyle`
     html,
     body{
+        @media screen and (max-width: 768px) {
+            width: 100%;
+        }
+        width: 768px;
         padding: 0px;
-        margin: 0px;
+        margin: 0px auto;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: flex-start;
         // 범위 확인용
         background : grey;
+
     }
 
 `
