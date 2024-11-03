c4d58acf359c5311bd020f3467ff14356297e03f
DevDAN09 <devyun09@gmail.com>
2024-11-03 15:46:01 +0900

    add header

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+
+const Header = styled.div`
+  height: 68px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+export default {
+  Header,
+  HeaderLeft,
+  HeaderRight,
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Styled from './Header.style';
+
+interface HeaderProps {
+  left?: React.ReactNode;
+  right?: React.ReactNode;
+}
+
+const Header = ({ left, right }: HeaderProps) => {
+  return (
+    <Styled.Header>
+      <Styled.HeaderLeft>{left}</Styled.HeaderLeft>
+      <Styled.HeaderRight>{right}</Styled.HeaderRight>
+    </Styled.Header>
+  );
+};
+
+export default Header;
