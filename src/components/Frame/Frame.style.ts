bd98dca60e743666de65e9c6d8197ec8e625b17d
seocylucky <seocylucky@github.com>
2024-11-05 02:42:23 +0900

    design: GlobalStyles 코드 수정 및 배경색 지정

--- src/components/Frame/Frame.style.ts
+++ src/components/Frame/Frame.style.ts
@@ -1,7 +1,51 @@
-import styled, { createGlobalStyle } from  "styled-components";
+import colors from '@/styles/color';
+import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-    html,
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        vertical-align: baseline;
+    }
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+    *{
+    box-sizing: border-box;
+    }
+    a{
+    text-decoration: none;
+    color: inherit;
+    }
     body{
         @media screen and (max-width: 768px) {
             width: 100%;
@@ -13,14 +57,13 @@ export const GlobalStyles = createGlobalStyle`
         flex-direction: column;
         align-items: center;
         justify-content: flex-start;
+        font-family: 'Pretendard Variable';
         // 범위 확인용
-        background : grey;
+        background : ${colors.background};
 
     }
-
-`
+`;
 
 export const FrameContainer = styled.div`
-    max-width: 100%;
-`
-
+  max-width: 100%;
+`;
