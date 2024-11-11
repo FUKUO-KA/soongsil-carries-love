a77847e5c74cf7b1af57ae4fe1751494864c02cb
seocylucky <seocylucky@github.com>
2024-11-12 03:29:11 +0900

    feat: TextField 컴포넌트 퍼블리싱 작업

---
+++ src/components/TextField/TextField.style.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import colors from '@/styles/color';
+import { getFontStyle } from '@/styles/typo';
+
+interface isErrorProps {
+  $isError: boolean;
+}
+
+export const StyledTextFieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+export const StyledTextFieldLabelContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+`;
+
+export const StyledTextFieldLabel = styled.label`
+  padding-left: 5px;
+  ${getFontStyle('Caption3')};
+  font-weight: 700;
+  color: ${colors.secondary_text};
+`;
+
+export const StyledTextFieldHelperText = styled.span<isErrorProps>`
+  ${getFontStyle('Caption3')};
+  font-weight: 700;
+  color: ${({ $isError }) => ($isError ? `${colors.sub_1}` : `${colors.secondary_text}`)};
+`;
+
+export const StyledTextFieldStarLabel = styled.span`
+  color: ${colors.sub_1};
+`;
+
+export const StyledTextFieldInput = styled.input<isErrorProps>`
+  width: 279px;
+  height: 39px;
+  padding: 13px 11px;
+  border-radius: 8px;
+  border: ${({ $isError }) => `1px solid ${$isError ? colors.sub_1 : colors.box_border}`};
+  background: ${colors.white};
+  box-sizing: border-box;
+
+  ${getFontStyle('Caption2')};
+  font-weight: 700;
+  color: ${colors.secondary_text};
+
+  &::placeholder {
+    color: #c7c7c7;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
