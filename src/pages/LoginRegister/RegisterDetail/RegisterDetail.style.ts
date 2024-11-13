1a9ac0d675055cfde4aefa1efbd30a4f16c55f06
seocylucky <seocylucky@github.com>
2024-11-13 22:34:37 +0900

    feat: 회원가입 상세 화면 분리

---
+++ src/pages/LoginRegister/RegisterDetail/RegisterDetail.style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledRegisterDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const StyledConfirmBtnContainer = styled.div`
+  padding: 46px;
+  position: absolute;
+  float: right;
+  right: -8px;
+`;
