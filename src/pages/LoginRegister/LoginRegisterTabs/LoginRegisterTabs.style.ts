966f5987e69be907222e11b53c8ef62601c3ac56
seocylucky <seocylucky@github.com>
2024-11-13 22:33:27 +0900

    feat: 로그인/회원가입 탭 분리 작업

---
+++ src/pages/LoginRegister/LoginRegisterTabs/LoginRegisterTabs.style.ts
@@ -0,0 +1,19 @@
+import colors from '@/styles/color';
+import styled from 'styled-components';
+
+export const StyledInfoText = styled.div`
+  color: ${colors.secondary_text};
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 6px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const StyledCertificateBtnContainer = styled.div`
+  padding: 22px;
+  position: absolute;
+  float: right;
+  right: 16px;
+`;
