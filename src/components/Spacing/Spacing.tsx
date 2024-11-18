76af7aaf3e264a0c4ffa5ef9d6138b0b2ae0ef83
seocylucky <seocylucky@github.com>
2024-11-18 16:27:51 +0900

    refactor: Spacing 컴포넌트 리팩토링(상수 분리)

--- src/components/Spacing/Spacing.tsx
+++ src/components/Spacing/Spacing.tsx
@@ -1,15 +1,12 @@
 import { HTMLAttributes, memo } from 'react';
 import { StyledSpacing } from './Spacing.style';
+import { DEFAULT_DIRECTION, Direction } from '@/constants/spacing.constant';
 
 interface SpacingProps extends HTMLAttributes<HTMLDivElement> {
-  direction?: 'horizontal' | 'vertical';
+  direction: Direction;
   size: number;
 }
 
-export const Spacing = memo(function Spacing({
-  direction = 'vertical',
-  size,
-  ...props
-}: SpacingProps) {
+export const Spacing = ({ direction = DEFAULT_DIRECTION, size, ...props }: SpacingProps) => {
   return <StyledSpacing direction={direction} size={size} {...props} />;
-});
+};
