98f381b48220a8f842c321cea64707ea776da59d
DevDan09 <devyun09@gmail.com>
2024-11-30 22:39:24 +0900

    feat: student-count api

---
+++ src/api/endpoints/hightschool/student-count.ts
@@ -0,0 +1,14 @@
+import APIAxiosInstance from '@/api/axios';
+
+export const studentCount = async (highschoolCode: string = '7010059') => {
+    const response = await APIAxiosInstance.get(`/highschool/student-count`, {
+        params: {
+            highschoolCode: highschoolCode
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': '*/*'
+        }
+    });
+    return response.data;
+};
